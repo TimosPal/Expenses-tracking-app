d3081e76b69275105286b382b5735dba03569ca2
TimosPal <timotheospalaiologos@gmail.com>
2024-08-16 14:44:59 +0300

    Added presets options functionality

--- src/components/DateRange.vue
+++ src/components/DateRange.vue
@@ -1,35 +1,97 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+function dateObjToStringFormat(date) {
+  return date.toISOString().split('T')[0]
+}
+
+function getStartOfTheWeekDate(date) {
+  // Weeks start on Monday.
+  const temp = new Date(date)
+  const offset = temp.getDay() === 0 ? 6 : temp.getDay() - 1
+  return temp.getDate() - offset
+}
+
+function handlePreset(presetID) {
+  if (presetID == '') {
+    return
+  }
+
+  activePreset.value = presetID
+
+  const fromDateObj = new Date()
+  const toDateObj = new Date()
+  switch (presetID) {
+    case 'today':
+      // Already set by default
+      break
+    case 'week':
+      // Start of the week.
+      fromDateObj.setDate(getStartOfTheWeekDate(fromDateObj))
+      break
+    case 'month':
+      // Start of the month
+      fromDateObj.setDate(1)
+      break
+    case 'year':
+      // Start of the year
+      fromDateObj.setDate(1)
+      fromDateObj.setMonth(0)
+      break
+    default:
+  }
+
+  fromDate.value = dateObjToStringFormat(fromDateObj)
+  toDate.value = dateObjToStringFormat(toDateObj)
+}
+
+// TODO: implement min, max values in date pickers.
+const fromDate = ref('')
+const toDate = ref('')
+const activePreset = ref('')
+handlePreset('month')
+</script>
 
 <template>
   <div class="date-range">
     <div class="presets">
-      <button type="button">Today</button>
-      <button type="button">Week</button>
-      <button type="button" class="activated">Month</button>
-      <button type="button">Year</button>
+      <button
+        type="button"
+        :class="{ activated: activePreset == 'today' }"
+        @click="handlePreset('today')"
+      >
+        Today
+      </button>
+      <button
+        type="button"
+        :class="{ activated: activePreset == 'week' }"
+        @click="handlePreset('week')"
+      >
+        Week
+      </button>
+      <button
+        type="button"
+        :class="{ activated: activePreset == 'month' }"
+        @click="handlePreset('month')"
+      >
+        Month
+      </button>
+      <button
+        type="button"
+        :class="{ activated: activePreset == 'year' }"
+        @click="handlePreset('year')"
+      >
+        Year
+      </button>
     </div>
     <div class="custom">
       <div class="from">
         <h4>From:</h4>
-        <input
-          type="date"
-          id="start"
-          name="trip-start"
-          value="2018-07-22"
-          min="2018-01-01"
-          max="2018-12-31"
-        />
+        <input type="date" id="from" v-model="fromDate" />
       </div>
       <div class="to">
         <h4>To:</h4>
-        <input
-          type="date"
-          id="start"
-          name="trip-start"
-          value="2018-07-22"
-          min="2018-01-01"
-          max="2018-12-31"
-        />
+        <input type="date" id="to" v-model="toDate" />
       </div>
     </div>
   </div>
