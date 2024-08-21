33fbf3ec25b96d3fe7d22add654af0a807e09eba
TimosPal <timotheospalaiologos@gmail.com>
2024-08-21 19:48:12 +0300

    removed unused css, small stylng to add new expense button

--- src/views/Expenses.vue
+++ src/views/Expenses.vue
@@ -23,74 +23,20 @@ const addExpense = () => {
 .new-expense-input {
   display: flex;
   flex-direction: row;
-  justify-content: end;
-  margin: 1rem;
+  justify-content: start;
+
+  margin-bottom: 0.5rem;
 
   button {
     all: unset;
-  }
+    background-color: $secondary-color;
+    color: $primary-color;
+    border-radius: 8px;
 
-  h4 {
-    margin: 1rem;
-  }
-}
+    padding: 0.5em;
 
-.table-main {
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  border-radius: 8px;
-  overflow: hidden;
-  background-color: #f9f9f9;
-
-  table {
-    width: 100%;
-    border-collapse: collapse;
-
-    thead {
-      background-color: $secondary-color;
-      color: $primary-color;
-
-      th {
-        padding: 1rem;
-        text-align: left;
-        font-weight: bold;
-        font-size: 1rem;
-      }
-
-      th:last-child {
-        text-align: right;
-        padding-right: 2rem;
-      }
-    }
-
-    tbody {
-      tr:nth-child(even) {
-        background-color: #f2f2f2;
-      }
-
-      tr:nth-child(odd) {
-        background-color: #ffffff;
-      }
-
-      td {
-        padding: 0.8rem 1rem;
-        border-bottom: 1px solid #ddd;
-        font-size: 1rem;
-        color: $secondary-color-shade;
-
-        button {
-          all: unset;
-          margin: 0 0.3rem;
-        }
-
-        &:last-child {
-          text-align: right;
-          padding-right: 2rem;
-        }
-      }
-
-      tr:last-child td {
-        border-bottom: none;
-      }
+    &:hover {
+      background-color: $secondary-color-shade;
     }
   }
 }
